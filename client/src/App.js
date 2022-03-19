addd790e1e472c6c7aaa908e3767f335da04542e
Jason Wisniewski <jwizinpa95@gmail.com>
2022-03-19 13:50:39 -0600

    bare bones, needs styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,58 @@
+import React, { useState } from 'react';
 import './App.css';
-import Nav from './components/Nav';
-import About from './components/About';
-import Contactme from './components/Contactme';
-import Portfolio from './components/Portfolio';
+// import Nav from './components/Nav';
+import About from './components/pages/About';
+// import Contact from './components/Contact';
+// import Portfolio from './components/Portfolio';
+import PortfolioContainer from "./components/PortfolioContainer";
 
-function App() {
-  return (
-    <div>
-      <Nav />
-      <About/>
-      <Contactme/>
-      <Portfolio/>
-    </div>
-  );
-}
+const App = () => <PortfolioContainer />;
+
+// function App() {
+//   const [projects] = useState([
+//   {
+//     name: 'brew hops',
+//     description: 'first project'
+//   },
+//   {
+//     name: 'planet express.js',
+//     description: 'second project'
+//   },
+//   {
+//     name: 'planet express.js',
+//     description: 'second project'
+//   },
+//   {
+//     name: 'planet express.js',
+//     description: 'second project'
+//   }
+//   ]);
+
+//   const [currentProject, setCurrentProject] = useState(projects[0]);
+  
+//   const [contactSelected, setContactSelected] = useState(false);
+
+//   return (
+//     <div>
+//       <Nav
+//         projects={projects}
+//         setCurrentProject={setCurrentProject}
+//         currentProject= {currentProject}
+//         contactSelected={contactSelected}
+//         setContactSelected={setContactSelected}
+//       ></Nav>
+//       <main>
+//         {!contactSelected ? (
+//           <>
+//             <Portfolio currentProject={currentProject}></Portfolio>
+//             <About></About>
+//           </>
+//         ) : (
+//           <Contact></Contact>
+//         )}
+//       </main>
+//     </div>
+//   );
+// }
 
 export default App;
